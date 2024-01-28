235bcff0a7becdc470557267ffef590c7aa2ad9f
Mussie <mussieabraham03@gmail.com>
2024-01-28 13:53:42 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x_value):
+        return x_value
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x_value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
     
 class Div:
     def __init__(self, p1, p2):
@@ -29,6 +38,13 @@ class Div:
         p1_repr = repr(self.p1) if not isinstance(self.p1, Add) else "( " + repr(self.p1) + " )"
         p2_repr = repr(self.p2) if not isinstance(self.p2, Add) else "( " + repr(self.p2) + " )"
         return p1_repr + " / " + p2_repr
+    
+    def evaluate(self, x_value):
+        divisor = self.p2.evaluate(x_value)
+        if divisor != 0:
+            return self.p1.evaluate(x_value) / divisor
+        else:
+            raise ZeroDivisionError("Error: Division by zero")
 
 class Sub:
     def __init__(self, p1, p2):
@@ -42,6 +58,9 @@ class Sub:
         p1_repr = repr(self.p1)
         p2_repr = wrap_if_needed(self.p2)
         return p1_repr + " - " + p2_repr
+    
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) - self.p2.evaluate(x_value)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -56,5 +75,8 @@ class Mul:
         p2_repr = wrap_div_sub(self.p2)
         return p1_repr + " * " + p2_repr
     
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
+    
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
