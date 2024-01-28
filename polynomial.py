695783ce18e6d0189f8f10dee88f3507977af0d6
Mussie <mussieabraham03@gmail.com>
2024-01-28 13:52:20 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -19,21 +19,42 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        p1_repr = repr(self.p1) if not isinstance(self.p1, Add) else "( " + repr(self.p1) + " )"
+        p2_repr = repr(self.p2) if not isinstance(self.p2, Add) else "( " + repr(self.p2) + " )"
+        return p1_repr + " / " + p2_repr
+
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        def wrap_if_needed(p):
+            return "( " + repr(p) + " )" if isinstance(p, (Add, Sub)) else repr(p)
+
+        p1_repr = repr(self.p1)
+        p2_repr = wrap_if_needed(self.p2)
+        return p1_repr + " - " + p2_repr
 
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        def wrap_div_sub(p):
+            return "( " + repr(p) + " )" if isinstance(p, (Add, Sub, Div)) else repr(p)
 
-
+        p1_repr = wrap_div_sub(self.p1)
+        p2_repr = wrap_div_sub(self.p2)
+        return p1_repr + " * " + p2_repr
+    
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
